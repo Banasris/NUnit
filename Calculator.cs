56376475095e29dbff14ea19921eff578a9de8e6
Banasris <banasri.saha@talk21.com>
2020-06-25 22:15:46 +0100

    first commit

---
+++ Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NUnits
+{
+    public class Calculator : IDisposable
+    {
+
+        public int AddNumns(int a, int b)
+        {
+            return(a + b);
+        }
+
+        public int SubtractNums(int a, int b)
+        {
+            if (a < b)
+                throw new Exception($"first number{a} is smaller than last number{b}");
+            return a - b;
+        }
+
+        public void Dispose() { }
+    }
+}
